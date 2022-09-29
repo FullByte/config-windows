42605660fc6b2a766369ef41a27fa609313ad224
0xfab1 <f@bi.an>
2022-09-29 11:21:46 +0200

    updates

--- tools/basics.ps1
+++ tools/basics.ps1
@@ -1,4 +1,14 @@
 
 
-choco install terraform
-choco install kubernetes-cli
+choco install -y terraform
+choco install -y kubernetes-cli
+
+
+Remove-Item Alias:Curl
+choco install -y curl
+choco install -y wget
+
+choco install exiftool
+choco install imagemagick
+choco install ffmpeg
+choco install pandoc
